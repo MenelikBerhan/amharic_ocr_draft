83d06ef97f6a48c15b7dd5de10487e095b78039f
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-12-02 04:29:31 +0300

    Add detailed USAGE string variable

--- functions/parse_input.py
+++ functions/parse_input.py
@@ -14,42 +14,64 @@ import argparse
 from pprint import pprint
 from shlex import split
 
-INPUT_DIR_DEFAULT_IMG = 'test_files/imgaes/'
+INPUT_DIR_DEFAULT_IMG = 'test_files/images/'
 INPUT_DIR_DEFAULT_PDF = 'test_files/pdfs/'
 OUTPUT_DIR_DEFAULT = 'test_files/outputs/'
 OUTPUT_MODE_DEFAULT = 'print'
 
-usage = """[image|pdf] INPUT_FILE [OUTPUT_FILE] [-d OUTPUT_DIR] [OPTION]...
-   or: [image|pdf] [-i] INPUT_FILE... [-jo OUTPUT_FILE] [-d OUTPUT_DIR] [OPTION]...
-   or: image|pdf -s INPUT_DIR [-jo OUTPUT_FILE] [-d OUTPUT_DIR] [OPTION]...
+usage = """[image|pdf] INPUT_FILE [OUTPUT_FILE] [OPTION]...
+   or: [image|pdf] -i INPUT_FILE... [-o OUTPUT_FILE] [OPTION]...
+   or: image|pdf -s INPUT_DIR [-o OUTPUT_FILE] [OPTION]...
 
-OCR INPUT_FILE or INPUT_FILES, and output to OUTPUT_FILE in OUTPUT_DIR.
-OCR INPUT_FILES in INPUT_DIR, and output to OUTPUT_FILE in OUTPUT_DIR.
+OCR INPUT_FILE/s and output to OUTPUT_FILE.
 
-Specifying image|pdf is mandatory only when passing INPUT_DIR using -s flag.
+OCR INPUT_FILE/s or all image|pdf files in INPUT_DIR,
+and output to OUTPUT_FILE.
 
-Multiple INPUT_FILEs can be passed without the -i flag.
+Multiple INPUT_FILEs can be passed with the -i flag. All inputs must be
+of the same file type. Types - image [*(.jpeg|.png|.jpg)] or pdf [*.pdf].
 
-Specifying -o flag for OUTPUT_FILE is madatory only when passing multiple inputs
-(with or without the -i flag) or when passing INPUT_DIR using -s flag.
+Specifying image|pdf prefix is mandatory only when passing INPUT_DIR
+without preceeding INPUT_FILE/s. Otherwise, will be inferred from
+INPUT_FILE/s extention.
 
-When passing OUTPUT_FILE along with multiple input files the -j flag must be used
-to join output files.
+If OUTPUT_FILE is passed along with multiple input files (using -i flag)
+and/or with INPUT_DIR (using -s flag), must use the -o flag.
 
 
 Mandatory arguments to long options are mandatory for short options too.
+
 -i, --input-files=INPUT_FILE...     pass multiple input files. Must use the -o flag for
-                                    OUTPUT_FILE if -i flag is used for input files.
--s, --src-dir=INPUT_DIR             use all images|pdfs in the specified INPUT_DIR directory
-                                    as input files. Must specify image|pdf. Must use the -o
-                                    flag for output file.
--o, --output-file=OUTPUT_FILE       save output with provided file name. Can pass destination
-                                    directory by prefixing it to OUTPUT_FILE.
--d, --dest-dir=OUTPUT_DIR           save output file in the specified OUTPUT_DIR directory.
+                                    OUTPUT_FILE.
+
+-s, --src-dir=INPUT_DIR             specify INPUT_FILE/s' directory. If no INPUT_FILE is
+                                    also passed, must specify image|pdf prefix and will use
+                                    all images|pdfs in INPUT_DIR directory as input files.
+
+                                    If INPUT_FILE/s is/are also passed, and is/are a path
+                                    (contains /), INPUT_DIR will be ignored.
+
+                                    Must use the -o flag for OUTPUT_FILE.
+
+-o, --output-file=OUTPUT_FILE       save output with provided file name. Implies -j flag.
+                                    Can pass destination directory by prefixing it to OUTPUT_FILE. 
+
+                                    If OUTPUT_FILE has a valid extention ('.txt'|'.docx'|'.pdf'),
+                                    will implicitly override output file type specified by -m.
+
+-d, --dest-dir=OUTPUT_DIR           save output file/s in the specified OUTPUT_DIR directory.
+                                    If OUTPUT_FILE is also passed and is a path (contains /),
+                                    OUTPUT_DIR will be ignored.
+
 -j, --join                          output to a single file. If not used when passing multiple
-                                    input files, an output file is created for each input file.
--m, --mode=['print', 'txt', 'pdf', 'docx']  specify output mode. Default 'print' to stdout.
-                                    'txt' to text, 'docx' to MSword and 'pdf' to pdf file.
+                                    input files without an OUTPUT_FILE, an output file is created
+                                    for each input file.
+
+-m, --mode='print'|'txt'|'pdf'|'docx'                   specify output mode (output file type).
+                                    'print' to stdout (default), 'txt' to text, 'docx' to MSword
+                                    and 'pdf' to pdf file.
+
+-v, --verbose                       display details of OCR (Confidence level, total pages ...)
 """
 
 
@@ -60,11 +82,11 @@ class ArgumentParser(argparse.ArgumentParser):
         def error(self, message):
             """Override default error handling to prevent default
             error display and system exit"""
-            print("ERRRRR")
+            # print("ERRRRR")
             raise(Exception(message))
 
 
-def parse_cmd(self, line):
+def parse_cmd(line):
     """Parses user input from command line and create a keyword dictionary.
     
     Args:
@@ -84,26 +106,28 @@ def parse_cmd(self, line):
         # usage=usage
         add_help=False  # so that -h flag doesn't display argparse help
         )
+
     parser.add_argument('input_file_type', choices=['image', 'pdf'], nargs='?')
 
     parser.add_argument('input_file', nargs='?')
     parser.add_argument('-i', '--input-file', dest='input_files', nargs='+', required=False, metavar='input_files')
 
-    parser.add_argument('-s', '--source-directory', dest='input_directory', nargs=1, required=False, default=INPUT_DIR_DEFAULT_IMG, metavar='source_directory')
+    parser.add_argument('-s', '--source-directory', dest='input_directory', nargs=1, required=False, default=INPUT_DIR_DEFAULT_IMG, metavar='input_directory')
 
     parser.add_argument('output_file', nargs='?')
     parser.add_argument('-o', '--output-file', dest='output_file', nargs=1, required=False, metavar='outptut_file')
 
-    parser.add_argument('-d', '--dest-directory', dest='output_directory', nargs=1, required=False, default=OUTPUT_DIR_DEFAULT, metavar='destination_directory')
+    parser.add_argument('-d', '--dest-directory', dest='output_directory', nargs=1, required=False, default=OUTPUT_DIR_DEFAULT, metavar='output_directory')
 
     parser.add_argument('-m', '--output-mode', dest='output_mode', choices=['print', 'txt', 'docx', 'pdf'], required=False, default=OUTPUT_MODE_DEFAULT, nargs=1, metavar='output_mode')
 
     parser.add_argument('-j', '--join', action='store_true', help='join outputs from multiple inputs into one file')
 
     parser.add_argument('-v', '--verbose', action='store_true', help='display details of OCR (Confidence level, total pages ...)')
+
     try:
         args = parser.parse_args(split(line))
-        print(args)
+        # print(args)
         pprint(vars(args))
     except Exception as e:
-        print('here', e.args)
+        print('*** Argument Error:', e)
