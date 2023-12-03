c7faab710175b192e161b295a087688de0297842
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-12-04 02:00:50 +0300

    Finish tesseract config dict creating function

--- functions/tesseract_config.py
+++ functions/tesseract_config.py
@@ -1,29 +1,34 @@
 #!/usr/bin/env python3
-import cv2
-from os import environ, path, remove
-import pytesseract as pts
-from sys import argv
-import docx
-from fpdf import FPDF
-from .validate_input import validate_input
+from os import environ
 
+TRAINING_DATA_DIR = '/home/menelikberhan/amharic_ocr/training_data_new'
 
 def config_tesseract(**args):
+    """Creates a string containing options to pass to tesseract based on args.
+    
+    Args:
+        **args (dict): dictionary containing custom parameters
 
+    Returns:
+        str: A string to be used for tesseract configuration.
+    """
 
-    training_folder = args.get('training_folder')
+    training_folder = args.get('training_folder')  # TODO check if it contains training data
+    
     # TRAINING DATA directory (if not provieded uses tesseracts default dir)
-    # TESSDATA_PREFIX='/home/menelikberhan/amharic_ocr/test_data' (doesnt work)
-    environ['TESSDATA_PREFIX'] = '/home/menelikberhan/amharic_ocr/training_data_new'
+    environ['TESSDATA_PREFIX'] = training_folder if training_folder else TRAINING_DATA_DIR
 
-    # environ['TESSDATA_PREFIX'] = '/home/menelikberhan/amharic_ocr/training_data'
 
     # config tesseract options
-    lang = 'amh'
-    psm = 1         # image processing option (1 & 3 for full page). refer doc.
-    options = """-l {} --psm {} --oem 1
+    # TODO add config params to **args from calling function & handle here
+    lang = args.get('lang', 'amh')
+    psm = args.get('psm', 3)        # image processing option (1 & 3 for full page). refer doc.
+    oem = args.get('oem', 1)        # refer doc
+    options = """
+    -l {} --psm {} --oem {}
     -c load_system_dawg=false
     -c load_freq_dawg=false
-    -c textord_space_size_is_variable=1""".format(lang, psm)
+    -c textord_space_size_is_variable=1
+    """.format(lang, psm, oem)
 
     return options
