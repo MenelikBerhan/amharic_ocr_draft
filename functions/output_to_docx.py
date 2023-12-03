42cdf536d8f450c0c2096ea15e19c721ec7751a8
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-12-04 02:00:07 +0300

    Finish writing to MS word feature

--- functions/output_to_docx.py
+++ functions/output_to_docx.py
@@ -12,12 +12,28 @@ import docx
 
 
 def write_to_docx(text, output_file_path, document=None, **args):
+    """Writes OCR'ed text to MS word document.
+    
+    Args:
+        text (str): output from OCR by tesseract.
+        output_file_path (str): path (including file name) of output file.
+        document (Document): a Document object loaded from docx to add pages to.
+            If not provided a new one will be created.
+        **args (dict): dictionary of parameters (font, layout ...)
+            for formatting document.
 
-    doc = document if document else docx.Document()
-    par = doc.add_paragraph().add_run(text)
+    Retruns:
+        Document: a Document object with newly added page containing text.
+
+    """
+
+    document = document if document else docx.Document()
+    par = document.add_paragraph().add_run(text)
     par.font.name = args.get('font_name', 'Abyssinica SIL')
-    if args.get('last_page') or args.get('save'):
-        doc.save(output_file_path)
 
+    if args.get('save'):
+        document.save(output_file_path)
+
+    return (document)
     # TODO - handle other args for MSword formatiing
 
